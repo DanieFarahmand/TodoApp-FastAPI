467709e9d8a545e572f724cb531c8664e92924c8
danie <danie.0098.sh@gmail.com>
2023-06-18 17:15:25 +0330

    some changes

--- routers/auth.py
+++ routers/auth.py
@@ -1,4 +1,10 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from starlette import status
+
+from database import SessionLocal
 from schema import CreateUserRequest
 import models
 from passlib.context import CryptContext
@@ -8,8 +14,19 @@ router = APIRouter()
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-@router.post("/auth/")
-async def create_user(create_user_request: CreateUserRequest):
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = models.Users(
         email=create_user_request.email,
         username=create_user_request.username,
@@ -19,4 +36,5 @@ async def create_user(create_user_request: CreateUserRequest):
         hashed_password=bcrypt_context.hash(create_user_request.password),
         is_active=True
     )
-    return create_user_model
+    db.add(create_user_model)
+    db.commit()
