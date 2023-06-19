97956bb5bed016fc2dccf24b7683ab3dfcd85d52
danie <danie.0098.sh@gmail.com>
2023-06-19 12:02:07 +0330

    authenticated_user method added for login proccess

--- routers/auth.py
+++ routers/auth.py
@@ -8,6 +8,7 @@ from database import SessionLocal
 from schema import CreateUserRequest
 import models
 from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
 
@@ -25,6 +26,15 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+def authenticate_user(username: str, password: str, db):
+    user = db.query(models.Users).filter(models.Users.username == username)
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
+
+
 @router.post("/auth", status_code=status.HTTP_201_CREATED)
 async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
     create_user_model = models.Users(
@@ -38,3 +48,12 @@ async def create_user(db: db_dependency, create_user_request: CreateUserRequest)
     )
     db.add(create_user_model)
     db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        return "Failed Authentication"
+    return "Successful Authentication"
--- routers/todos.py
+++ routers/todos.py
@@ -28,7 +28,7 @@ async def read_all_todos(db: db_dependency):
 
 @router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
 async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
-    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    todo_model = db.query(models.Todos).get(todo_id).first()
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail="Todo not found")
@@ -44,7 +44,7 @@ async def create_todo(db: db_dependency, todo_request: TodoRequest):
 @router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_todo(db: db_dependency, todo_request: TodoRequest,
                       todo_id: int = Path(gt=0)):
-    todo_model = db.query(models.Todos).filter(models.Todos.id == todo_id).first()
+    todo_model = db.query(models.Todos).get(todo_id).first()
     if todo_model is None:
         raise HTTPException(status_code=404, detail="Todo is not found")
     update_data = {
