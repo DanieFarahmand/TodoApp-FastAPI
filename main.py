153705da5e61ee1102889c181b8d705948bcd60e
danie <danie.0098.sh@gmail.com>
2023-06-24 22:25:07 +0330

    admin and users router created

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI
 
-from routers import auth, todos
+from routers import auth, todos, admin, users
+
 import models
 from database import engine
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
-# ..\venv\Scripts\activate
-
-# uvicorn main:app --reload
 
 app.include_router(auth.router)
 app.include_router(todos.router)
-
+app.include_router(admin.router)
+app.include_router(users.router)
